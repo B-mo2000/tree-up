92057c5af0aab065eeb586765d77b9e564bc6eb3
B-mo <msh300msh600@gmail.com>
2024-03-24 12:57:27 +0200

    tree alpha 1

---
+++ ttt/auto.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var trees = 100
+var one_time = true
+var grow_no = false
---
+++ ttt/tree.gd
@@ -0,0 +1,54 @@
+extends Marker2D
+
+var trees : PackedScene = load("res://ttt/tree.tscn")
+
+var random = RandomNumberGenerator.new()
+var r = 1
+
+const rotation_max = 35
+
+func ttt() -> void:
+	var t = trees.instantiate()
+	t.position = $tree_pos.position
+	add_child(t)
+	if Auto.one_time == false:
+		rotation_degrees = random.randi_range(-rotation_max,rotation_max)
+
+func ttt2() -> void:
+	for i in 2:
+		var t = trees.instantiate()
+		t.position = $tree_pos.position
+		add_child(t)
+		rotation_degrees = random.randi_range(-rotation_max,rotation_max)
+
+func ttt3() -> void:
+	for i in 3:
+		var t = trees.instantiate()
+		t.position = $tree_pos.position
+		add_child(t)
+		rotation_degrees = random.randi_range(-rotation_max,rotation_max)
+
+func _ready():
+	random.randomize()
+	if Auto.one_time == false:
+		if Auto.trees >= 90:
+			r = random.randi_range(1,2)
+		if Auto.trees < 90:
+			r = random.randi_range(1,3)
+	await (get_tree().create_timer(.3).timeout)
+	
+	if Auto.trees > 0:
+		Auto.trees -= 1
+		
+		if r == 1:
+			ttt()
+		if r == 2:
+			ttt2()
+		#if r == 3:
+			#ttt3()
+	
+	Auto.one_time = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
